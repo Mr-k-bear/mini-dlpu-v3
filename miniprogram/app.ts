9ba327407267ecc32378ee41403c82c5d6960d5e
MrKBear <mrkbear@qq.com>
2021-11-25 16:11:29 +0800

    Initialize mini programs configuration

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,18 +1,14 @@
 // app.ts
 App<IAppOption>({
-  globalData: {},
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
+  /**
+   * 全局数据
+   */
+  globalData: {},
+
+  /**
+   * 小程序加载时
+   */
+  onLaunch() {
   },
 })
