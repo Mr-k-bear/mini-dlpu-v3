55d52f446faef9315c3c0741b88df70e92116dd4
MrKBear <mrkbear@qq.com>
2021-12-01 16:14:10 +0800

    Optimize Manger so that it can load data

--- miniprogram/pages/Timetable/Timetable.ts
+++ miniprogram/pages/Timetable/Timetable.ts
@@ -2,52 +2,62 @@ import { Logger } from "../../core/Logger";
 import { LevelLogLabel, LifeCycleLogLabel } from "../../core/PresetLogLabel";
 import { Manager, Modular, AnyWXContext, ILifetime } from "../../core/Module";
 
-Page({
+let manager;
 
-    /**
-     * 课程表页面加载
-     */
-    onLoad: async function (query) {
+// Page({
 
-        this;
+//     /**
+//      * 课程表页面加载
+//      */
+//     onLoad: async function (query) {
 
-        let manager = new Manager(this);
-        let m1 = manager.addModule(M1, "m1");
-        let m2 = manager.addModule(M2, "m2", {m1});
+//         manager = new Manager(this);
+//         console.log(manager);
 
-        manager.loadAllModule(query);
 
-        this.setData;
+//         // this.setData;
 
-        Logger.log("课程表 (Timetable) 页面加载...", 
-        LevelLogLabel.TraceLabel, LifeCycleLogLabel.OnLoadLabel);
+//         // Logger.log("课程表 (Timetable) 页面加载...", 
+//         // LevelLogLabel.TraceLabel, LifeCycleLogLabel.OnLoadLabel);
 
-        let systemInfo = wx.getSystemInfoSync();
+//         // let systemInfo = wx.getSystemInfoSync();
  
-        //状态栏高度
-        let statusBarHeight = Number(systemInfo.statusBarHeight);
+//         // //状态栏高度
+//         // let statusBarHeight = Number(systemInfo.statusBarHeight);
         
-        let menu = wx.getMenuButtonBoundingClientRect()
+//         // let menu = wx.getMenuButtonBoundingClientRect()
         
-        //导航栏高度
-        let navBarHeight = menu.height + (menu.top - statusBarHeight) * 2
+//         // //导航栏高度
+//         // let navBarHeight = menu.height + (menu.top - statusBarHeight) * 2
         
-        //状态栏加导航栏高度
-        let navStatusBarHeight = statusBarHeight + menu.height + (menu.top - statusBarHeight) * 2
+//         // //状态栏加导航栏高度
+//         // let navStatusBarHeight = statusBarHeight + menu.height + (menu.top - statusBarHeight) * 2
         
-        console.log('状态栏高度',statusBarHeight)
+//         // console.log('状态栏高度',statusBarHeight)
         
-        console.log('导航栏高度',navBarHeight)
+//         // console.log('导航栏高度',navBarHeight)
         
-        console.log('状态栏加导航栏高度',navStatusBarHeight)
+//         // console.log('状态栏加导航栏高度',navStatusBarHeight)
 
-        this.setData({barh: navStatusBarHeight});
+//         // this.setData({barh: navStatusBarHeight});
 
-    }
+//     }
   
+// })
+
+
+Manager.Page((manager)=>{
+    let m1 = manager.addModule(M1, "m1");
+    let m2 = manager.addModule(M2, "m2", {m1});
 })
 
+
 class M1<M extends Manager> extends Modular<M, {}> implements Partial<ILifetime> {
+
+    data = {
+        a:1,
+        b:{}
+    }
 
     public onLoad(){
 
@@ -60,7 +70,11 @@ class M1<M extends Manager> extends Modular<M, {}> implements Partial<ILifetime>
 }
 
 class M2<M extends Manager> extends Modular<M, {m1:M1<M>}> {
-    
+
+    data = {
+        a: 1
+    }
+
     public onLoad() {
         this.setData({a:1});
         this.depends?.m1.on("lll", (e)=>{
