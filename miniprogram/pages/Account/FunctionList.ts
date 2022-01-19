49055e892c4573608c5ee2ea9df68366f8189995
mrkbear <mrkbear@qq.com>
2022-01-19 21:55:55 +0800

    (#37) Optimization function list

--- miniprogram/pages/Account/FunctionList.ts
+++ miniprogram/pages/Account/FunctionList.ts
@@ -1,9 +1,49 @@
 import { Modular, Manager } from "../../core/Module";
 
+interface IFunctionListItem {
+
+    /**
+     * id
+     */
+    id?: number
+
+    /**
+     * 显示名称
+     */
+    displayName: string;
+
+    /**
+     * 图标路径
+     */
+    iconUrl: string;
+}
+
+interface IFunctionListData {
+    functionList?: IFunctionListItem[];
+};
+
 class FunctionList<M extends Manager> extends Modular<M> {
+
+    public static readonly functionList: IFunctionListItem[] = [
+        { displayName: "赞助计划", iconUrl: "Sponsor" },
+        { displayName: "公众号", iconUrl: "PubilcAccount" },
+        { displayName: "自助问答", iconUrl: "FAQ" },
+        { displayName: "关于我们", iconUrl: "AboutUs" },
+        { displayName: "联系客服", iconUrl: "Support" }
+    ];
+
+    public data: IFunctionListData = {
+        functionList: undefined
+    };
     
     public override onLoad() {
-        // Do something
+        console.log(FunctionList.functionList)
+        this.setData({
+            functionList: FunctionList.functionList.map((value, index) => {
+                value.id = index;
+                return value;
+            })
+        })
     }
 }
 
