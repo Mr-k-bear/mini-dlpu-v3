63300f68f8f13ec93a1c4618a414ca05ea34004f
MrKBear <mrkbear@qq.com>
2022-01-20 13:26:24 +0800

    (#37) Optimize Account page

--- miniprogram/pages/Account/MainFunction.ts
+++ miniprogram/pages/Account/MainFunction.ts
@@ -1,6 +1,30 @@
 import { Modular, Manager } from "../../core/Module";
 
+interface IMainFunctionItem {
+
+    /**
+     * 显示名称
+     */
+    displayName: string;
+
+    /**
+     * 图标路径
+     */
+    iconUrl: string;
+}
+
 class MainFunction<M extends Manager> extends Modular<M> {
+
+    public static readonly MainFunctionList: IMainFunctionItem[] = [
+        { displayName: "账号信息", iconUrl: "UserInfo" },
+        { displayName: "课表缓存", iconUrl: "DateList" },
+        { displayName: "功能定制", iconUrl: "Customer" },
+        { displayName: "更多设置", iconUrl: "Settings" }
+    ];
+
+    public data? = {
+        mainFunctionList: MainFunction.MainFunctionList
+    }
     
     public override onLoad() {
         // Do something
