87f4d220e53269c5fb18a88eb26c55c415f397e4
MrKBear <mrkbear@qq.com>
2022-01-20 11:52:10 +0800

    (#45) Optimize Modular loading performance

--- miniprogram/pages/Account/Account.ts
+++ miniprogram/pages/Account/Account.ts
@@ -4,9 +4,23 @@ import { MainFunction } from "./MainFunction";
 import { FunctionList } from "./FunctionList";
 import { Mask } from "../../modular/Mask/Mask";
 
-Manager.Page((manager) => {
+(async () => {
+
+    // 初始化页面
+    const { manager, query } = await Manager.PageAsync();
+
+    // 添加蒙版 Modular
     const mask = manager.addModule(Mask, "mask");
+
+    // 添加 UserCard Modular
     manager.addModule(UserCard, "userCard", { mask });
+
+    // 添加 MainFunction Modular
     manager.addModule(MainFunction, "mainFunction");
+
+    // 添加 FunctionList Modular
     manager.addModule(FunctionList, "functionList");
-});
+
+    // 初始化全部 Modular
+    await manager.loadAllModule(query);
+})();
--- miniprogram/pages/Timetable/Timetable.ts
+++ miniprogram/pages/Timetable/Timetable.ts
@@ -6,7 +6,17 @@ import { TestCore } from "./TestCore";
  * 此页面使用 Manager 进行模块化管理
  * 若要添加先功能请先定义 Modular 并添加至 Manager
  */
-Manager.Page((manager)=>{
+(async () => {
+
+    // 初始化页面
+    const { manager, query } = await Manager.PageAsync();
+
+    // 添加 StatusBar Modular
     manager.addModule(StatusBar, "statusBar");
+    
+    // 添加 TestCore Modular
     manager.addModule(TestCore, "testCore");
-})
+
+    // 初始化全部 Modular
+    await manager.loadAllModule(query);
+})()
