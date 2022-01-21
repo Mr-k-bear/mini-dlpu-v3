49851d4a57494857852ee0a597580d4dbee3e34f
yzy <1279165429@qq.com>
2022-01-21 20:11:11 +0800

    (#36) Add popups

--- miniprogram/modular/Popups/Popups.ts
+++ miniprogram/modular/Popups/Popups.ts
@@ -2,28 +2,50 @@ import { Modular, Manager } from "../../core/Module";
 
 class Popups<M extends Manager> extends Modular<M> {
 
+     /**
+     * 动画运行时间
+     */
+    public static readonly animateTime: number = 100;
+
     public data? = {
         
         /**
 		 *  弹出层是否显示
 		 */
-		isShow: false
-    };
+        display:false,
 
-    private disappearTimer?: number;
+        /**
+         * 弹出层动画
+         */
+        isShow:false
+    };
+    
+    disappearTimer: number | undefined;
 
     /**
 	 * 显示弹出层
 	 */
 	public showPopups() {
-		this.setData({ isShow: true });
+    this.setData({
+      isShow:true,
+      display:true
+    });
 	}
 
 	/**
 	 * 隐藏弹出层
 	 */
 	public hidePopups() {
-		this.setData({ isShow: false });
+		this.setData({
+            isShow:false
+        });
+        
+        this.disappearTimer = setTimeout(() => {
+            this.setData({ 
+                display: false
+            });
+        }, Popups.animateTime);
+
 	}
 	
 	public override onLoad() {
